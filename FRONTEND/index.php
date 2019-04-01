d6648168a041a0a854428ae073e68595050b81b3
NSanovsky <38820841+NSanovsky@users.noreply.github.com>
2019-04-01 20:12:54 +0200

    ++ index.php ++ footer.php u to_include folderu (u tom folderu je sve što se ponavlja kao footer, navbar i ikonice pa se samo includa tamo gdje treba)

---
+++ FRONTEND/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>  
+    
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,600,700i,800,800i,900,900i|Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
+    
+    <script src="main.js"></script>
+</head>
+<body>
+    <?php
+    include('to_include/footer.php')
+    ?>
+</body>
+</html>
