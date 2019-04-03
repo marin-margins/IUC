b97ff8a5f43b9b84800487175f3bf8af25c63eff
NSanovsky <38820841+NSanovsky@users.noreply.github.com>
2019-04-03 18:08:27 +0200

    ++ Dodane slicice u to_include ++ U index.php dodan navbar, header, cover photo itd itd ++ zasad vise manje responzivno, treba jos malo sredit

--- FRONTEND/index.php
+++ FRONTEND/index.php
@@ -13,6 +13,166 @@
     <script src="main.js"></script>
 </head>
 <body>
+    <div id="donate">
+    <div id="donate-navs">
+        <a class="donate-nav" href="" ><svg   xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="30.5" height="33" viewBox="0 0 30.5 33"><defs><style>.ap{fill:#3891cc;}.b{filter:url(#a);}</style><filter id="a" x="0" y="0" width="30.5" height="33" filterUnits="userSpaceOnUse"><feOffset input="SourceAlpha"/><feGaussianBlur stdDeviation="3" result="b"/><feFlood flood-color="#3891cc"/><feComposite operator="in" in2="b"/><feComposite in="SourceGraphic"/></filter></defs><g class="b" transform="matrix(1, 0, 0, 1, 0, 0)"><path class="ap" d="M7.851,1.25C10.449,1.25,9.5,5,9.5,5s3.75-1.031,3.75,1.536V13.75h-10V1.25h4.6ZM8.368,0H2V15H14.5V6.009C14.5,4.514,10.345,0,8.368,0Zm3.007,8.125H5.125V7.5h6.25Zm0,1.25H5.125V10h6.25Zm0,1.875H5.125v.625h6.25Z" transform="translate(7 9)"/></g></svg>IUC Publications</a>
+        <a class="donate-nav" href=""><svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="33" height="33" viewBox="0 0 33 33"><defs><style>.aj{fill:#3891cc;}.b{filter:url(#a);}</style><filter id="a" x="0" y="0" width="33" height="33" filterUnits="userSpaceOnUse"><feOffset input="SourceAlpha"/><feGaussianBlur stdDeviation="3" result="b"/><feFlood flood-color="#3891cc"/><feComposite operator="in" in2="b"/><feComposite in="SourceGraphic"/></filter></defs><g class="b" transform="matrix(1, 0, 0, 1, 0, 0)"><path class="aj" d="M4.455,14.1,0,15l.9-4.453ZM1.783,9.662l3.555,3.556L15,3.556,11.445,0,1.784,9.662Z" transform="translate(9 9)"/></g></svg>IUC Journal</a>
+    </div>
+        <a id="donate-btn"> DONATE <svg xmlns="http://www.w3.org/2000/svg" width="10" height="9.166" viewBox="0 0 10 9.166"><defs><style>.adb{fill:#fff;}</style></defs><path class="adb" d="M5,2.353C3.688.1,0,.759,0,3.58c0,1.942,2.321,3.928,5,6.587,2.679-2.659,5-4.645,5-6.587C10,.75,6.3.119,5,2.353Z" transform="translate(0 -1)"/></svg></a>
+    </div>
+
+
+    <!-- NAVBAR -->
+    <div class="navbar">
+        <div class="nav-left">
+        <img src="to_include/logo-header.png" alt="IUC International University Centre Dubrovnik Logo">
+        <span id="divider"></span>
+        <p>Inter-University Centre Dubrovnik<br><span id="IUC-subtitle">An independent international centre for advanced studies</span></p>
+        </div>
+
+        <div class="nav-right">
+            
+            <input type="search" placeholder="search" name="" id="search">
+            <svg class="search-icon" xmlns="http://www.w3.org/2000/svg" width="15.207" height="15.207" viewBox="0 0 15.207 15.207"><defs><style>.asrt,.csrt{fill:none;}.asrt{stroke:#8b8b8b;stroke-width:2px;}.bsrt{stroke:none;}</style></defs><g class="asrt"><circle class="bsrt" cx="6.5" cy="6.5" r="6.5"/><circle class="csrt" cx="6.5" cy="6.5" r="5.5"/></g><line class="asrt" x2="4" y2="4" transform="translate(10.5 10.5)"/></svg>
+            <a><svg xmlns="http://www.w3.org/2000/svg" width="16" height="18" viewBox="0 0 16 18"><defs><style>.as{fill:none;stroke:#010727;stroke-width:2px;}</style></defs><g transform="translate(0 1)"><line class="as" x2="16"/><line class="as" x2="16" transform="translate(0 8)"/><line class="as" x2="16" transform="translate(0 16)"/></g></svg></a>
+        </div>
+
+        
+    </div>
+
+    <div class="nav-mobile">
+          <p>Inter-University Centre Dubrovnik<br><span id="IUC-subtitle">An independent international centre for advanced studies</span></p>
+    </div>    
+
+
+    <!-- HEADER -->
+
+    <div class="header">
+      
+        <div class="img"></div>
+        <div class="overlay"></div>
+        <div class="content-wrap">
+            <div class="content">
+                <span class="blue-line"></span>
+                <h1>Lorem ipsum dolor</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br>Yeiusmod tempor incididunt ut labore et dolore magna aliqua.<br> Ut enim ad minim veniam</p>
+                <a href="" class="btn">MORE ABOUT IUC</a>
+            </div>
+         
+        </div>
+        <div class="circles">
+                <span class="circle"></span>
+                <span class="circle"></span>
+                <span class="circle"></span>
+                <span class="circle"></span>
+        </div>
+        <div class="header-icons">
+            <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15"><defs><style>.aing{fill:#fff;}</style></defs><path class="aing" d="M11.875,0H3.125A3.125,3.125,0,0,0,0,3.125v8.75A3.125,3.125,0,0,0,3.125,15h8.75A3.125,3.125,0,0,0,15,11.875V3.125A3.125,3.125,0,0,0,11.875,0ZM10,4.375H8.8c-.384,0-.673.157-.673.556v.694H10L9.851,7.5H8.125v5H6.25v-5H5V5.625H6.25v-1.2c0-1.264.665-1.923,2.163-1.923H10Z"/></svg></a>
+            <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15"><defs><style>.afc{fill:#fff;}</style></defs><path class="afc" d="M7.5,1.352c2,0,2.24.008,3.031.044A2.818,2.818,0,0,1,13.606,4.47c.036.791.043,1.028.043,3.031s-.007,2.24-.043,3.031c-.093,2.016-1.04,2.982-3.074,3.074-.791.036-1.028.044-3.031.044s-2.24-.007-3.031-.044c-2.038-.093-2.982-1.062-3.074-3.075C1.359,9.74,1.351,9.5,1.351,7.5s.008-2.239.044-3.031C1.488,2.453,2.435,1.488,4.469,1.4,5.261,1.359,5.5,1.352,7.5,1.352ZM7.5,0C5.463,0,5.208.009,4.408.045A4.14,4.14,0,0,0,.046,4.408C.009,5.208,0,5.463,0,7.5s.009,2.293.045,3.092a4.14,4.14,0,0,0,4.363,4.363c.8.036,1.056.045,3.092.045s2.293-.009,3.092-.045a4.14,4.14,0,0,0,4.362-4.363c.037-.8.046-1.056.046-3.092s-.009-2.292-.045-3.092A4.14,4.14,0,0,0,10.593.046C9.792.009,9.537,0,7.5,0Zm0,3.649A3.852,3.852,0,1,0,11.351,7.5,3.851,3.851,0,0,0,7.5,3.649ZM7.5,10A2.5,2.5,0,1,1,10,7.5,2.5,2.5,0,0,1,7.5,10Zm4-7.4a.9.9,0,1,0,.9.9A.9.9,0,0,0,11.5,2.6Z"/></svg></a>
+        </div>
+
+
+
+    </div>
+
+    <div class="collapsible-box">
+        <div class="collapsible ">
+            <div class="title">EXPLORE OUR COURSES & CONFERENCES <svg class="arrow" xmlns="http://www.w3.org/2000/svg" width="7.785" height="11.326" viewBox="0 0 7.785 11.326"><defs><style>.ane{fill:none;stroke:#010727;stroke-width:3px;}</style></defs><path class="ane" d="M3299.6,405l-4.6,4.6,4.6,4.6" transform="translate(3300.664 415.266) rotate(180)"/></svg></div>
+            <div class="box-content">
+
+            </div>
+        </div>
+        <div class="collapsible active">
+        <div class="title">OR BROWSE THROUGH FIELDS OF YOUR INTEREST <svg class="arrow" xmlns="http://www.w3.org/2000/svg" width="7.785" height="11.326" viewBox="0 0 7.785 11.326"><defs><style>.ane{fill:none;stroke:#010727;stroke-width:3px;}</style></defs><path class="ane" d="M3299.6,405l-4.6,4.6,4.6,4.6" transform="translate(3300.664 415.266) rotate(180)"/></svg></div>
+            <div class="box-content">
+                <div class="row">
+                    <div class="row-item">
+                        <div class="item">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="10" viewBox="0 0 20 10"><defs><style>.abc{fill:#42444b;}</style></defs><path class="abc" d="M15,16H5l0-.519c0-1.049.083-1.655,1.323-1.942,1.4-.324,2.787-.613,2.121-1.841C6.472,8.063,7.882,6,10,6c2.076,0,3.522,1.986,1.555,5.7-.646,1.22.688,1.51,2.121,1.841,1.242.287,1.324.893,1.324,1.944L15,16Zm4.009-1.845c-1.075-.248-2.075-.466-1.591-1.381C18.89,9.989,17.807,8.5,16.249,8.5a1.788,1.788,0,0,0-1.873,1.937c0,3.253,1.89,1.475,1.872,5.563H20l0-.386c0-.788-.062-1.244-.993-1.459ZM0,16H3.752c-.017-4.088,1.872-2.31,1.872-5.563A1.788,1.788,0,0,0,3.751,8.5c-1.558,0-2.641,1.489-1.166,4.274.484.916-.516,1.133-1.591,1.381C.062,14.37,0,14.826,0,15.614L0,16Z" transform="translate(0 -6)"/></svg>
+                            <p>Behavioural studies, Society and Religion </p>
+                        </div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="7.785" height="11.326" viewBox="0 0 7.785 11.326"><defs><style>.anss{fill:none;stroke:#3891cc;stroke-width:3px;}</style></defs><path class="anss" d="M3299.6,405l-4.6,4.6,4.6,4.6" transform="translate(3300.664 415.266) rotate(180)"/></svg>
+                    </div>
+                    <div class="row-item">
+                        <div class="item">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10"><defs><style>.alw{fill:none;stroke:#42444b;stroke-width:2px;}.b{fill:#42444b;}</style></defs><g transform="translate(-720 -900)"><line class="alw" y2="3" transform="translate(725 900)"/><path class="ba" d="M6410,900l2.5,3,2.5-3Z" transform="translate(-5690 7)"/><path class="ba" d="M6410,900l2.5,3,2.5-3Z" transform="translate(-5685 7)"/><line class="alw" x1="6" transform="translate(722 903)"/><path class="alw" d="M0,0V3" transform="translate(722 902)"/><line class="alw" y2="3" transform="translate(728 902)"/></g></svg>
+                            <p>Law, Economics, Politics and Governance</p>
+                        </div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="7.785" height="11.326" viewBox="0 0 7.785 11.326"><defs><style>.anss{fill:none;stroke:#3891cc;stroke-width:3px;}</style></defs><path class="anss" d="M3299.6,405l-4.6,4.6,4.6,4.6" transform="translate(3300.664 415.266) rotate(180)"/></svg>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="row-item">
+                        <div class="item">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="8.813" height="10" viewBox="0 0 8.813 10"><defs><style>.ae{fill:#42444b;}</style></defs><path class="ae" d="M9.131,12.56a2.939,2.939,0,0,0,.415,2.571,7.989,7.989,0,0,1,3.76-3.039A9.082,9.082,0,0,0,9.4,19h1.011a6.387,6.387,0,0,1,.5-2.934,2.855,2.855,0,0,0,2.8-.613c1.724-1.463,1.077-5.14,4.112-5.564-4.07-2.224-7.943.124-8.683,2.671Z" transform="translate(-9.002 -9)"/></svg>
+                            <p>Earth and Sustainability</p>
+                        </div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="7.785" height="11.326" viewBox="0 0 7.785 11.326"><defs><style>.anss{fill:none;stroke:#3891cc;stroke-width:3px;}</style></defs><path class="anss" d="M3299.6,405l-4.6,4.6,4.6,4.6" transform="translate(3300.664 415.266) rotate(180)"/></svg>
+                    </div>
+                    <div class="row-item">
+                        <div class="item">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10"><defs><style>.am{fill:#42444b;}.bm{fill:#fff;stroke:#42444b;}.cm{stroke:none;}.dm{fill:none;}</style></defs><g transform="translate(-707 -979)"><circle class="am" cx="2" cy="2" r="2" transform="translate(707 979)"/><circle class="am" cx="2" cy="2" r="2" transform="translate(713 985)"/><g class="bm" transform="translate(707.527 986.338) rotate(-43)"><rect class="cm" width="10" height="2"/><rect class="dm" x="0.5" y="0.5" width="9" height="1"/></g></g></svg>
+                        <p>Mathematics, Science and Technology</p>
+                        </div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="7.785" height="11.326" viewBox="0 0 7.785 11.326"><defs><style>.anss{fill:none;stroke:#3891cc;stroke-width:3px;}</style></defs><path class="anss" d="M3299.6,405l-4.6,4.6,4.6,4.6" transform="translate(3300.664 415.266) rotate(180)"/></svg>
+                    </div>         
+
+                </div>
+
+                <div class="row">
+                    <div class="row-item">
+                        <div class="item">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="10" viewBox="0 0 12 10"><defs><style>.aH{fill:#42444b;fill-rule:evenodd;}</style></defs><path class="aH" d="M9.452,8C8.438,9.2,7.021,10.5,5.5,12,2.553,9.1,0,6.933,0,4.814,0,1.737,4.057,1.021,5.5,3.476,6.932,1.039,11,1.727,11,4.814A3.162,3.162,0,0,1,10.751,6H7.612a.35.35,0,0,0-.3.179L6.9,6.9,5.766,4.685a.35.35,0,0,0-.6-.04L4.227,6H3.613a1,1,0,1,0,0,1h.978a.351.351,0,0,0,.286-.148l.494-.7L6.5,8.428a.35.35,0,0,0,.613.026L7.993,7H12V8Z" transform="translate(0 -2)"/></svg>
+                        <p>Health and Life Sciences</p>
+                        </div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="7.785" height="11.326" viewBox="0 0 7.785 11.326"><defs><style>.anss{fill:none;stroke:#3891cc;stroke-width:3px;}</style></defs><path class="anss" d="M3299.6,405l-4.6,4.6,4.6,4.6" transform="translate(3300.664 415.266) rotate(180)"/></svg>
+                    </div>
+                    <div class="row-item">
+                        <div class="item">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10"><defs><style>.aM{fill:#42444b;}</style></defs><path class="aM" d="M5.627,14.5c2.889.262,1.855-3.108,4.528-3.141l1.157,1.062A3.076,3.076,0,0,1,5.627,14.5Zm10-6.156L13.22,6a9.45,9.45,0,0,0-2.478,4.592l1.175,1.077A8.418,8.418,0,0,0,15.627,8.343Z" transform="translate(-5.627 -6)"/></svg>
+                        <p>Media, Arts, Culture and History</p>
+                        </div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="7.785" height="11.326" viewBox="0 0 7.785 11.326"><defs><style>.anss{fill:none;stroke:#3891cc;stroke-width:3px;}</style></defs><path class="anss" d="M3299.6,405l-4.6,4.6,4.6,4.6" transform="translate(3300.664 415.266) rotate(180)"/></svg>
+                    </div>         
+
+                </div>
+
+                <div class="row">
+                    <div class="row-item">
+                        <div class="item">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="7" viewBox="0 0 10 7"><defs><style>.aL{fill:#42444b;}</style></defs><rect class="aL" width="10" height="7"/></svg>
+                        <p>Languages and Communication</p>
+
+
+                        </div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="7.785" height="11.326" viewBox="0 0 7.785 11.326"><defs><style>.anss{fill:none;stroke:#3891cc;stroke-width:3px;}</style></defs><path class="anss" d="M3299.6,405l-4.6,4.6,4.6,4.6" transform="translate(3300.664 415.266) rotate(180)"/></svg>
+                    </div>
+                    <div class="row-item">
+                        <div class="item">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10"><defs><style>.aP,.cP{fill:none;}.aP{stroke:#42444b;stroke-width:2px;}.bP{stroke:none;}</style></defs><g transform="translate(-720 -1130)"><line class="aP" y2="10" transform="translate(725 1130)"/><g class="aP" transform="translate(720 1132)"><rect class="bP" width="10" height="6" rx="3"/><rect class="cP" x="1" y="1" width="8" height="4" rx="2"/></g></g></svg>
+                        <p>Philosophy and Philosophy of Science</p>
+                        </div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="7.785" height="11.326" viewBox="0 0 7.785 11.326"><defs><style>.anss{fill:none;stroke:#3891cc;stroke-width:3px;}</style></defs><path class="anss" d="M3299.6,405l-4.6,4.6,4.6,4.6" transform="translate(3300.664 415.266) rotate(180)"/></svg>   
+                    </div>         
+
+                </div>
+
+
+
+            </div>
+        </div>
+
+
+
+    </div>
+
+    <div class="upcoming">
+
+    </div>
+
+
+
     <?php
     include('to_include/footer.php')
     ?>
