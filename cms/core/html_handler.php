c2e068d470b20283e57283eb817cfb0e51939e03
progonitelj <31577031+progonitelj@users.noreply.github.com>
2019-05-08 20:03:57 +0200

    ++ hotfix

--- cms/core/html_handler.php
+++ cms/core/html_handler.php
@@ -48,23 +48,21 @@ class html_handler
               </div>
 
               <!-- Bootstrap core JavaScript-->
-              <script src="vendor/jquery/jquery.min.js"></script>
+              <script src="vendor/jquery/jquery.js"></script>
               <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
               <!-- Core plugin JavaScript-->
               <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
               <!-- Page level plugin JavaScript-->
-              <script src="vendor/chart.js/Chart.min.js"></script>
+      
               <script src="vendor/datatables/jquery.dataTables.js"></script>
               <script src="vendor/datatables/dataTables.bootstrap4.js"></script>
 
               <!-- Custom scripts for all pages-->
               <script src="js/sb-admin.min.js"></script>
 
-              <!-- Demo scripts for this page-->
-              <script src="js/demo/datatables-demo.js"></script>
-              <script src="js/demo/chart-area-demo.js"></script>
+             
 
             </body>
 
@@ -94,6 +92,8 @@ class html_handler
 
                       <!-- Custom styles for this template-->
                       <link href="css/sb-admin.css" rel="stylesheet">
+                      
+                      <script src="vendor/jquery/jquery.js"></script>
 
                     </head>
 
