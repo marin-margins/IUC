45346b3f5c0251dbb575448178ee5a9fdf084701
NSanovsky <38820841+NSanovsky@users.noreply.github.com>
2019-05-12 15:44:05 +0200

    ++ dodan news folder

---
+++ FRONTEND/news/index.php
@@ -0,0 +1,56 @@
+<?php var_dump($_SERVER)?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>  
+    
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="/IUC/trunk/FRONTEND/style.css?ts=<?=time()?>"/>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,600,700i,800,800i,900,900i|Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
+    
+    
+</head>
+<body>
+    <div id="donate">
+    <div id="donate-navs">
+        <a class="donate-nav" href="" ><svg   xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="30.5" height="33" viewBox="0 0 30.5 33"><defs><style>.ap{fill:#3891cc;}.b{filter:url(#a);}</style><filter id="a" x="0" y="0" width="30.5" height="33" filterUnits="userSpaceOnUse"><feOffset input="SourceAlpha"/><feGaussianBlur stdDeviation="3" result="b"/><feFlood flood-color="#3891cc"/><feComposite operator="in" in2="b"/><feComposite in="SourceGraphic"/></filter></defs><g class="b" transform="matrix(1, 0, 0, 1, 0, 0)"><path class="ap" d="M7.851,1.25C10.449,1.25,9.5,5,9.5,5s3.75-1.031,3.75,1.536V13.75h-10V1.25h4.6ZM8.368,0H2V15H14.5V6.009C14.5,4.514,10.345,0,8.368,0Zm3.007,8.125H5.125V7.5h6.25Zm0,1.25H5.125V10h6.25Zm0,1.875H5.125v.625h6.25Z" transform="translate(7 9)"/></g></svg>IUC Publications</a>
+        <a class="donate-nav" href=""><svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="33" height="33" viewBox="0 0 33 33"><defs><style>.aj{fill:#3891cc;}.b{filter:url(#a);}</style><filter id="a" x="0" y="0" width="33" height="33" filterUnits="userSpaceOnUse"><feOffset input="SourceAlpha"/><feGaussianBlur stdDeviation="3" result="b"/><feFlood flood-color="#3891cc"/><feComposite operator="in" in2="b"/><feComposite in="SourceGraphic"/></filter></defs><g class="b" transform="matrix(1, 0, 0, 1, 0, 0)"><path class="aj" d="M4.455,14.1,0,15l.9-4.453ZM1.783,9.662l3.555,3.556L15,3.556,11.445,0,1.784,9.662Z" transform="translate(9 9)"/></g></svg>IUC Journal</a>
+    </div>
+        <a id="donate-btn"> DONATE <svg xmlns="http://www.w3.org/2000/svg" width="10" height="9.166" viewBox="0 0 10 9.166"><defs><style>.adb{fill:#fff;}</style></defs><path class="adb" d="M5,2.353C3.688.1,0,.759,0,3.58c0,1.942,2.321,3.928,5,6.587,2.679-2.659,5-4.645,5-6.587C10,.75,6.3.119,5,2.353Z" transform="translate(0 -1)"/></svg></a>
+    </div>
+
+
+    <!-- NAVBAR -->
+    <!-- NAVBAR -->
+    <div class="navbar course-navbar">
+        <div class="nav-left">
+          <img src="../to_include/logo-header.png" style="width: 45px; height: 21px; margin-top: 5px" alt="IUC International University Centre Dubrovnik Logo">
+          <p>Inter-University Centre <span>Dubrovnik</span></p>
+        </div>
+
+        <div class="nav-right">
+
+            <input type="search" placeholder="search" name="" id="search">
+            <svg class="search-icon course-search-icon" xmlns="http://www.w3.org/2000/svg" width="15.207" height="15.207" viewBox="0 0 15.207 15.207"><defs><style>.asrt,.csrt{fill:none;}.asrt{stroke:#8b8b8b;stroke-width:2px;}.bsrt{stroke:none;}</style></defs><g class="asrt"><circle class="bsrt" cx="6.5" cy="6.5" r="6.5"/><circle class="csrt" cx="6.5" cy="6.5" r="5.5"/></g><line class="asrt" x2="4" y2="4" transform="translate(10.5 10.5)"/></svg>
+            <a><svg xmlns="http://www.w3.org/2000/svg" width="16" height="18" viewBox="0 0 16 18"><defs><style>.as{fill:none;stroke:#010727;stroke-width:2px;}</style></defs><g transform="translate(0 1)"><line class="as" x2="16"/><line class="as" x2="16" transform="translate(0 8)"/><line class="as" x2="16" transform="translate(0 16)"/></g></svg></a>
+        </div>
+
+
+    </div>
+
+    <div class="nav-mobile">
+          <p>Inter-University Centre Dubrovnik<br><span id="IUC-subtitle">An independent international centre for advanced studies</span></p>
+    </div>
+
+
+    <!-- HEADER -->
+
+    
+    <?php
+    include('../to_include/footer.php')
+    ?>
+</body>
+</html>
