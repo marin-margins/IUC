6bd6ee966edf32660214e3f66523ad5f27dead87
MarinMijalic <46677885+MarinMijalic@users.noreply.github.com>
2019-05-26 18:51:40 +0200

    cijela tablica

--- cms/persons.php
+++ cms/persons.php
@@ -81,6 +81,12 @@ if(!empty($_POST['FrName']) && !empty($_POST['LaName']) && !empty($_POST["select
                             <th scope="col">Instition</th>
                         </tr>
                         </thead>
+                        <tfoot>
+                        <th scope="col">First name</th>
+                        <th scope="col">Last name</th>
+                        <th scope="col">Country</th>
+                        <th scope="col">Instition</th>
+                        </tfoot>
 
                         <tbody>
 
@@ -88,12 +94,29 @@ if(!empty($_POST['FrName']) && !empty($_POST['LaName']) && !empty($_POST["select
                     </table>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-sm-12 col-md-6">
+                    <div class="dataTables_info" id="dataTable_info" role="status" aria-live="polite">Showing 1 to 10 of 57 entries</div>
+                </div><div class="col-sm-12 col-md-6">
+                    <div class="dataTables_paginate paging_simple_numbers" id="dataTable_paginate">
+                        <ul class="pagination">
+                            <li class="paginate_button page-item previous disabled" id="dataTable_previous">
+                                <a href="#" aria-controls="dataTable" data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li>
+                            <li class="paginate_button page-item active"><a href="#" aria-controls="dataTable" data-dt-idx="1" tabindex="0" class="page-link">1</a></li>
+                            <li class="paginate_button page-item "><a href="#" aria-controls="dataTable" data-dt-idx="2" tabindex="0" class="page-link">2</a></li>
+                            <li class="paginate_button page-item "><a href="#" aria-controls="dataTable" data-dt-idx="3" tabindex="0" class="page-link">3</a></li>
+                            <li class="paginate_button page-item "><a href="#" aria-controls="dataTable" data-dt-idx="4" tabindex="0" class="page-link">4</a></li>
+                            <li class="paginate_button page-item "><a href="#" aria-controls="dataTable" data-dt-idx="5" tabindex="0" class="page-link">5</a></li>
+                            <li class="paginate_button page-item "><a href="#" aria-controls="dataTable" data-dt-idx="6" tabindex="0" class="page-link">6</a></li>
+                            <li class="paginate_button page-item next" id="dataTable_next"><a href="#" aria-controls="dataTable" data-dt-idx="7" tabindex="0" class="page-link">Next</a></li>
+                        </ul></div></div></div>
         </div>
     </div>
     <div class="col-md-3 col-md-offset-1">
         <form method="POST" action="persons.php" id="forma">
             <div class="form-group">
                 <div class="card-header">
+                    <i class="fas fa-table"></i>
                     <i class="fas fa-table"></i>
                     Persons details
                 </div>
